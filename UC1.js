75a474d10ac450f4453a7a302cf996273f76f672
RIYA_BANSAL24 <riyabansal3365@gmail.com>
2025-03-20 19:37:32 +0530

    Checkif emp is present or not

---
+++ UC1.js
@@ -0,0 +1,8 @@
+const isAbsent=0
+let emp=Math.floor(Math.random()*10)%2;
+if(emp==isAbsent){
+    console.log("Employee is Absent");
+    return;
+}else{
+    console.log("Employee is Present");
+}
